8c033a81b010067d3d17ef956661096c5be36ff3
fkincir <fkincir@kpmg.com>
2023-06-30 16:49:25 +0200

    httpclient module get post request to fisebase database

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductRepository } from '../models/product.repository';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'product-list',
@@ -9,13 +10,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  products: Product[];
+  products: Product[] = [];
 
   productRepository: ProductRepository;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.productRepository = new ProductRepository();
-    this.products = this.productRepository.getProducts();
+    // this.products = this.productRepository.getProducts();
   }
 
   ngOnInit(): void {
@@ -25,14 +26,24 @@ export class ProductListComponent implements OnInit {
         this.products =
           this.productRepository.getProductByCategoryId(categoryId);
       } else {
-        this.products = this.productRepository.getProducts();
+        // this.products = this.productRepository.getProducts();
+        this.http
+          .get<Product[]>(
+            'https://ng-shopapp-47866-default-rtdb.firebaseio.com/products.json'
+          )
+          .subscribe((data) => {
+            console.log(data);
+            const result: Product[] = [];
+            for (const key in data) {
+              // console.log(key);
+              // console.log(data[key]);
+              // console.log({ ...data[key], id: key });
+              result.push({ ...data[key], id: key });
+            }
+            // console.log(this.products);
+            this.products = result;
+          });
       }
     });
   }
-  // selectProduct(product: Product) {
-  //   this.selectedProduct = product;
-  // }
-  // unselectProduct() {
-  //   this.selectedProduct = null;
-  // }
 }
