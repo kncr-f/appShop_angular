24b0cf7dd058a31225c3b50c674c4ed64676cca0
fkincir <fkincir@kpmg.com>
2023-07-12 19:00:50 +0200

    showing product_details with product.service

--- src/app/product-list/product-detail/product-detail.component.ts
+++ src/app/product-list/product-detail/product-detail.component.ts
@@ -2,24 +2,29 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductRepository } from 'src/app/models/product.repository';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
+  providers: [ProductService],
 })
 export class ProductDetailComponent implements OnInit {
   product: Product | undefined;
-  productRepository: ProductRepository;
 
-  constructor(private route: ActivatedRoute) {
-    this.productRepository = new ProductRepository();
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((data) => {
       const id = data['productId'];
-      this.product = this.productRepository.getProductById(id);
+
+      this.productService.getProductById(id).subscribe((data) => {
+        this.product = { ...data, id: id };
+      });
     });
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -7,16 +7,19 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class ProductService {
-  private url =
-    'https://ng-shopapp-47866-default-rtdb.firebaseio.com/products.json';
+  private url = 'https://ng-shopapp-47866-default-rtdb.firebaseio.com/';
 
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.url);
+    return this.http.get<Product[]>(this.url + 'products.json');
   }
 
   createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.url, product);
+    return this.http.post<Product>(this.url + 'products.json', product);
+  }
+
+  getProductById(id: string): Observable<Product> {
+    return this.http.get<Product>(this.url + 'products/' + id + '.json');
   }
 }
