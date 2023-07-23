f70de78f39fc4fd8f79569a17077a14ce96bb6b1
fkincir <fkincir@kpmg.com>
2023-07-23 15:57:20 +0200

    add product functionality added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailComponent } from './product-list/product-detail/product-detail.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 const routes: Routes = [
+  { path: 'products/createProduct', component: CreateProductComponent },
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'products/:productId', component: ProductDetailComponent },
